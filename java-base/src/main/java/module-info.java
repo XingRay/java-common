2ce004d9df3cfdc222dbbdafe4573e7b6e688152
leixing <leixing1012@gmail.com>
2023-02-03 19:45:18 +0800

    1. exports Page

--- java-base/src/main/java/module-info.java
+++ java-base/src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module com.xingray.java.base {
     exports com.xingray.java.base.interfaces;
-    exports com.xingray.java.base.result;
     exports com.xingray.java.base.range;
 
+    exports com.xingray.java.base.result;
     opens com.xingray.java.base.result;
+
+    exports com.xingray.java.base.page;
 }
