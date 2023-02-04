7a75c12770158ec35d41ec1ea1d4e4f656447fe1
leixing <leixing1012@gmail.com>
2023-02-04 22:50:57 +0800

    1. 增加 TimePage

---
+++ java-base/src/main/java/com/xingray/java/base/page/TimePage.java
@@ -0,0 +1,44 @@
+package com.xingray.java.base.page;
+
+import java.util.List;
+
+public class TimePage<T> {
+    private int total;
+
+    private long timestamp;
+
+    private List<T> dataList;
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public List<T> getDataList() {
+        return dataList;
+    }
+
+    public void setDataList(List<T> dataList) {
+        this.dataList = dataList;
+    }
+
+    @Override
+    public String toString() {
+        return "TimePage{" +
+                "total=" + total +
+                ", timestamp=" + timestamp +
+                ", dataList=" + dataList +
+                '}';
+    }
+}
