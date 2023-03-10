5b58283ac98d688cf6bac5be56f155560f6e67aa
leixing <leixing1012@gmail.com>
2023-03-11 02:04:31 +0800

    1. 删除 installer 模块

--- java-installer/src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module com.xingray.java.installer {
-    requires com.xingray.java.command;
-
-    exports com.xingray.java.installer;
-    opens com.xingray.java.installer;
-}
--- java-installer/src/test/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.xingray.java.installer.test {
-    requires org.junit.jupiter;
-    requires com.xingray.java.installer;
-    requires com.xingray.java.command;
-    requires org.slf4j;
-
-    exports com.xingray.java.installer.test;
-}
